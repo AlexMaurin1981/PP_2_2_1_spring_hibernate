d073145c9c53d23ecde3082aaadb94645b7a200a
AlexMaurin1981 <155809065+AlexMaurin1981@users.noreply.github.com>
2024-03-24 14:08:24 +0300

    finish

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,11 +15,14 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
-
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car1 = new Car("AUDI", 4);
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",car1));
+      Car car2 = new Car("BMW", 5);
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",car2));
+      Car car3 = new Car("LADA", 2101);
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",car3));
+      Car car4 = new Car("ZICR", 001);
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",car4));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,6 +30,24 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println();
+      }
+       List<User>showUser= userService.userBySeriesAndModel("ZICR", 001);
+      for (User user: showUser){
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " +user.getCar());
+         System.out.println();
+      }
+      List<User>showUser2= userService.userBySeriesAndModel("LADA", 2101);
+      for (User user: showUser2){
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " +user.getCar());
          System.out.println();
       }
 
